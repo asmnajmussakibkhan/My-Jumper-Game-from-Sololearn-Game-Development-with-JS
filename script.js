64432c8650a238bfe22b2d66cb37eacf7208b048
ASM Najmus Sakib Khan <72307730+asmnajmussakibkhan@users.noreply.github.com>
2023-08-08 00:52:19 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,53 @@
 window.onload = function() {
-    var btn = document.getElementById("jump");
+    let count = 0;
     var canvas = document.getElementById("canvas");
     var context = canvas.getContext("2d");
     var x = 300;
     var y = 350;
-    var count = 0;
-    btn.onclick = function() {
-        count += 1;
-        y -= 25;
-        context.clearRect(0, 0, canvas.width, canvas.height);
-        context.beginPath();
-        context.arc(x, y, 50, 0, 2 * Math.PI);
-        context.fillStyle = "red";
-        context.fill();
-        context.font = '25px Arial';
-        context.fillStyle = 'white';
-        context.fillText("Count: " + count, 20, 30);
-    }
-    context.beginPath();
+    var t = Date.now();
+    var timepassed=(Date.now()-t)/1000;
+    var speed = 150;
     context.arc(x, y, 50, 0, 2 * Math.PI);
-    context.fillStyle = "red";
+    context.fillStyle="red";
     context.fill();
+    function draw() {
+    var timepassed=(Date.now()-t)/1000;
+    t=Date.now();
+    var speed = 25;
+    if(y<=350){
+    speed+=75*timepassed;
+    y+=speed*timepassed;
+    }
+    if(y>350){
+        count=0
+    }
+    context.clearRect(0, 0, 600, 400);
+    context.beginPath();
+    context.arc(x, y, 50, 0, 2*Math.PI);
+    context.fillStyle="red";
+    context.fill();
+    context.font='25px Arial';
+    context.fillStyle='limegreen';
+    context.fillText("Count: " + count, 20, 30);
+    window.requestAnimationFrame(draw);
+}
+document.onkeydown=function(){
+    count +=1;
+    y-=25;
+    speed=25;
+    draw()
+}
+document.ontouchstart=function(){
+    count+=1;
+    y-=25;
+    speed=25;
+    draw()
+}
+document.onmousedown=function(){
+    count+=1;
+    y-=25;
+    speed=25;
+    draw()
+}
+draw()
 }
