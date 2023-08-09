8234f70c51fb4bfd0b3d557196b4836c483b6537
ASM Najmus Sakib Khan <72307730+asmnajmussakibkhan@users.noreply.github.com>
2023-08-09 19:16:58 +0600

    Update script.js

--- script.js
+++ script.js
@@ -8,18 +8,18 @@ window.onload = function() {
     var timepassed=(Date.now()-t)/1000;
     var speed = 150;
     context.arc(x, y, 50, 0, 2 * Math.PI);
-    context.fillStyle="red";
+    context.fillStyle = "red";
     context.fill();
     function draw() {
-    var timepassed=(Date.now()-t)/1000;
-    t=Date.now();
+    var timepassed = (Date.now()-t)/1000;
+    t = Date.now();
     var speed = 25;
-    if(y<=350){
-    speed+=75*timepassed;
-    y+=speed*timepassed;
+    if (y<=350) {
+        speed += 50 * timepassed;
+        y += speed * timepassed;
     }
-    if(y>350){
-        count=0
+    if (y>350) {
+        count = 0
     }
     context.clearRect(0, 0, 600, 400);
     context.beginPath();
@@ -31,22 +31,22 @@ window.onload = function() {
     context.fillText("Count: " + count, 20, 30);
     window.requestAnimationFrame(draw);
 }
-document.onkeydown=function(){
-    count +=1;
-    y-=25;
-    speed=25;
+document.onkeydown = function(){
+    count += 1;
+    y -= 25;
+    speed = 25;
     draw()
 }
-document.ontouchstart=function(){
-    count+=1;
-    y-=25;
-    speed=25;
+document.ontouchstart = function(){
+    count += 1;
+    y -= 25;
+    speed = 25;
     draw()
 }
-document.onmousedown=function(){
-    count+=1;
-    y-=25;
-    speed=25;
+document.onmousedown = function(){
+    count += 1;
+    y -= 25;
+    speed = 25;
     draw()
 }
 draw()
